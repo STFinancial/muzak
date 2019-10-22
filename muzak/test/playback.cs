b7616d24be9a87d39506a64e175b50103a8c568d
stfinancial <palla021@umn.edu>
2019-10-21 22:27:11 -0500

    Testing NAudio package. Couldn't get GTK to work so will have to try that later.

---
+++ muzak/test/playback.cs
@@ -0,0 +1,33 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace muzak.test
+{
+    class playback
+    {
+        void Thing()
+        {
+            using (var audioFile = new AudioFileReader(@"G:\\Code\muzak\muzak\youGotmail.wav"))
+            using(var outputDevice = new WaveOutEvent())
+            {
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+                while (outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            playback p = new playback();
+            p.Thing();
+        }
+    }
+
+}

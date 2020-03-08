e8eff39cc60a643f7373b56d0f6f65cd44f7911c
stfinancial <palla021@umn.edu>
2020-03-07 21:54:22 -0600

    Decided on SQLite

--- muzak/media/Library.cs
+++ muzak/media/Library.cs
@@ -1,11 +1,24 @@
 ﻿using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
 
 namespace muzak.media
 {
-    /** The Library encompasses the full collection of Trakcs as well as all generated Playlists */
+    /** The Library encompasses the full collection of Tracks as well as all generated Playlists */
     class Library
     {
+        // Figure out how to reference this location properly.
+        private static readonly string LIBRARY_LOCATION = "G:\\Code\\muzak\\library.sqlite";
         Playlist tracks;
         List<Playlist> playlists;
+
+        void initialize()
+        {
+            if(!File.Exists(LIBRARY_LOCATION))
+            {
+                SQLiteConnection.CreateFile(LIBRARY_LOCATION);
+
+            }
+        }
     }
 }
